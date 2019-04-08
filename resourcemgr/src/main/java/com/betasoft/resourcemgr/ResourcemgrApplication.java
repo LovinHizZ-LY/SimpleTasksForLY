0b8a3fb6602c3e4fafa3841c56c59c866dfbff06
LovinHizZ-LY <liuyongv587@163.com>
2019-04-08 16:07:16 +0800

    初步构建

---
+++ resourcemgr/src/main/java/com/betasoft/resourcemgr/ResourcemgrApplication.java
@@ -0,0 +1,13 @@
+package com.betasoft.resourcemgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcemgrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourcemgrApplication.class, args);
+    }
+
+}
---
+++ resourcemgr/src/test/java/com/betasoft/resourcemgr/ResourcemgrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.betasoft.resourcemgr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResourcemgrApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
